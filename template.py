49a88e31ba3292cfc3ca18b5fd400c4332629056
saint122 <rohitkhatri404@gmail.com>
2024-01-05 03:09:19 +0530

    file creation

---
+++ template.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+list_of_file = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "setup.py",
+    "research/trials.ipynb",
+    "app.py",
+    "store_index.py",
+    "static",
+    "templates/chat.html"
+
+]
+
+for filepath in list_of_file:
+    filepath = Path(filepath) #converting filepath to path \ /
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir,exist_ok = True)
+        logging.info(f"creating directory: {filedir}for the file {filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filename) ==0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} is alreade created")
+
+
